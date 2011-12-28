638b32b4a50a10583b9ed488c47bc5c776a20305
Jack Culhane <jackculhane@gmail.com>
2011-12-28 23:48:01 +0000

    Added the GPL header

--- clever_chat.c
+++ clever_chat.c
@@ -1,5 +1,19 @@
-#include "clever_chat.h"
+//Copyright (C) 2011 Jack Culhane - jackculhane@gmail.com
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#include "clever_chat.h"
 
 //This function replaces any 0Ds with 0As in a string
 void cr2lf( char *string ){
@@ -474,10 +488,10 @@ static PurplePluginInfo info = {
     PURPLE_PRIORITY_DEFAULT,                             //Priority
 
     "core-clever_chat",                                  //ID
-    "Clever Chat",                                       //Name
+    "CleverChat",                                       //Name
     "0.1",                                               //Version
 
-    "Clever Chat Plugin",                                //Summary
+    "CleverChat Plugin",                                //Summary
 
     "Allows you to send a contact's messages"
     "to Cleverbot and have Cleverbot respond.",          //Description
